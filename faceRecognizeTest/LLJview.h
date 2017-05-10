09405cde46b59d210db27e86169f60a25e70cb30
13240811187 <807261242@qq.com>
2017-05-10 09:37:37 +0800

    修改

---
+++ faceRecognizeTest/LLJview.h
@@ -0,0 +1,13 @@
+//
+//  LLJview.h
+//  faceRecognizeTest
+//
+//  Created by Crab on 2017/5/10.
+//  Copyright © 2017年 Crab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LLJview : UIView
+
+@end
---
+++ faceRecognizeTest/LLJview.m
@@ -0,0 +1,21 @@
+//
+//  LLJview.m
+//  faceRecognizeTest
+//
+//  Created by Crab on 2017/5/10.
+//  Copyright © 2017年 Crab. All rights reserved.
+//
+
+#import "LLJview.h"
+
+@implementation LLJview
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
